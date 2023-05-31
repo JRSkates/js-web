40320be43ac7c1696201763ba7f9cf831b26a1df
JRSkates <skatesproduction@gmail.com>
2023-05-31 16:40:20 +0100

    Notes Functionality Complete

--- notes-app/notesView.js
+++ notes-app/notesView.js
@@ -3,18 +3,33 @@ class NotesView {
     this.model = model;
     this.mainContainerEl = document.querySelector('#main-container');
     this.addNoteButtonEl = document.querySelector('#add-note-button');
+    this.clearAllNotesButtonEl = document.querySelector('#clear-notes-button');
 
     this.addNoteButtonEl.addEventListener('click', () => {
       this.displayNotes();
     });
+
+    this.clearAllNotesButtonEl.addEventListener('click', () => {
+      this.clearNotes();
+    });
   }
 
   displayNotes() {
-    const inputEl = document.querySelector('#note-input');
-    const noteEl = document.createElement('div');
-    noteEl.className = 'note';
-    noteEl.textContent = inputEl.value;
-    this.mainContainerEl.append(noteEl);
+    const notes = this.model.getNotes();
+    notes.forEach(element => {
+      element = document.querySelector('#note-input').value;
+      const noteEl = document.createElement('div');
+      noteEl.className = 'note';
+      noteEl.textContent = element;
+      this.mainContainerEl.append(noteEl);
+    });
+    document.getElementById('note-input').value = "";
+  }
+
+  clearNotes() {
+    document.querySelectorAll('.note').forEach(element => {
+      element.remove();
+    });
   }
 
 }
--- notes-app/notesView.test.js
+++ notes-app/notesView.test.js
@@ -19,4 +19,46 @@ describe('NotesView', () => {
 
     expect(document.querySelectorAll('div.note').length).toBe(1);
   });
+
+  it ('clears the list of previous note before displaying', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel
+    const view = new NotesView(model)
+    const inputEl = document.querySelector('#note-input');
+    inputEl.value = 'test input';
+    model.addNote(inputEl.value)
+    inputEl.value = 'test two input';
+    model.addNote(inputEl.value)
+
+    view.displayNotes();
+    //view.displayNotes();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(2);
+  });
+
+  it ('clears the input value after adding a note', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel
+    const view = new NotesView(model)
+    const inputEl = document.querySelector('#note-input');
+    inputEl.value = 'test input';
+    view.displayNotes();
+
+    expect(document.getElementById('note-input').value).toBe("")
+  });
+
+  it ('clears my notes with the Clear Notes button', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const model = new NotesModel
+    const view = new NotesView(model)
+    const inputEl = document.querySelector('#note-input');
+    inputEl.value = 'test input';
+    view.displayNotes();
+    view.clearNotes();
+
+    expect(document.querySelectorAll('div.note').length).toEqual(0);
+  });
 });
